f17ee8bc388d451db2532eb1ca5399e52f9ee7c1
Thiyobuilds <Thiyo80203@gmail.com>
2025-06-25 00:01:48 -0200

    Local Storage Pending

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 let productImg = document.getElementById("productImg");
 let productTitle = document.getElementById("productTitle");
 let productInfo = document.getElementById("productInfo");
-let productPrice = document.getElementById("ProductPrice")
+let productPrice = document.getElementById("ProductPrice");
 let genBtn = document.getElementById("genBtn");
 
 let cartsDiv = document.createElement("div");
 cartsDiv.classList.add("carts");
 
-genBtn.addEventListener("click",()=>{
+genBtn.addEventListener("click", () => {
 
-let imgTag = document.createElement("img");
-let titleTag = document.createElement("h3");
-let pTag = document.createElement("p");
-let priceTag = document.createElement("h4");
-let decreBtn = document.createElement("button");
-let count = document.createElement("span")
-let IncreBtn = document.createElement("button");
+    if([productImg,productTitle,productInfo,productPrice].some(input => input.value.trim() === "" )){
+         return alert('Please Fill The Input Field!')
+    }
+    else{
 
-let cartDiv = document.createElement("div");
-cartDiv.classList.add("cart")
+  let imgTag = document.createElement("img");
+  let titleTag = document.createElement("h3");
+  let pTag = document.createElement("p");
+  let priceTag = document.createElement("h4");
+  let decreBtn = document.createElement("button");
+  let count = document.createElement("span");
+  let IncreBtn = document.createElement("button");
 
-    imgTag.src = productImg.value;
-    cartDiv.append(imgTag);
+  let cartDiv = document.createElement("div");
+  cartDiv.classList.add("cart");
 
-    titleTag.textContent = productTitle.value;
-    cartDiv.append(titleTag);
+  imgTag.src = productImg.value;
+  cartDiv.append(imgTag);
 
-    pTag.textContent = productInfo.value;
-    cartDiv.append(pTag);
+  titleTag.textContent = productTitle.value;
+  cartDiv.append(titleTag);
 
-    cartsDiv.appendChild(cartDiv);
-    document.body.append(cartsDiv);
+  pTag.textContent = productInfo.value;
+  cartDiv.append(pTag);
 
-})
+  let addCart = document.createElement("div");
+  addCart.classList.add("addCart");
+
+  priceTag.textContent = `$${productPrice.value}`;
+  addCart.append(priceTag);
+
+  let countSec = document.createElement("div");
+  countSec.classList.add("incre");
+
+  decreBtn.textContent = "-";
+  countSec.append(decreBtn);
+
+  count.textContent = " "+0+" ";
+  countSec.append(count);
+
+  IncreBtn.textContent = "+";
+  countSec.append(IncreBtn);
+
+  addCart.append(countSec);
+  cartDiv.append(addCart);
+
+  cartsDiv.appendChild(cartDiv);
+  document.body.append(cartsDiv);
+
+  productImg.value = "";
+  productTitle.value = "";
+  productInfo.value = "";
+  productPrice.value = "";
+    }
+
+});
